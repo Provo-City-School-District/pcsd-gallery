c64dc9020d19b007a1db14181a8c2b51f6f8ab3b
Espi <joshespi@gmail.com>
2022-09-26 14:32:20 -0600

    made image entry required to eliminate empty entries.

--- pcsd-gallery/pcsd-gallery-fields.php
+++ pcsd-gallery/pcsd-gallery-fields.php
@@ -31,7 +31,7 @@ if( function_exists('acf_add_local_field_group') ):
                         'name' => 'image',
                         'type' => 'image',
                         'instructions' => '',
-                        'required' => 0,
+                        'required' => 1,
                         'conditional_logic' => 0,
                         'wrapper' => array(
                             'width' => '',
